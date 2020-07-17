74813ec13656e3d9bbf50b02819c61eb0c9e1eb8
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-07-17 15:23:29 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name="pdns",
+    packages=find_packages(),
+    # metadata to display on PyPI
+    author="Me",
+    author_email="me@example.com",
+    description="This is an Example Package",
+    keywords="hello world example examples",
+    url="http://example.com/HelloWorld/",   # project home page, if any
+    project_urls={
+        "Bug Tracker": "https://bugs.example.com/HelloWorld/",
+        "Documentation": "https://docs.example.com/HelloWorld/",
+        "Source Code": "https://code.example.com/HelloWorld/",
+    },
+    classifiers=[
+        "License :: OSI Approved :: Python Software Foundation License"
+    ],
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+)
